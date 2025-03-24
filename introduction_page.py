3a1118309d97e7ea880e1a233a948dcad1942f1c
t-koeppen <koeppen.tamea@gmail.com>
2025-03-24 20:23:31 +0100

    Add files via upload

---
+++ introduction_page.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def introduction_page():
+        st.image("images/StyleGuide.png")
+        st.header("What is this app?")
+
+        st.markdown("Hello and welcome to this Style Guide! You probably found this guide because you are interested in"
+                    " different styles. Well congrats, you found the right app!🪩 With this app you get to know "
+                    "different styles. With the menu on the top left side you can navigate to a styles page, a chatbot,"
+                    " a page with tips, and some info about me, the creator.")
+
+        st.markdown("On the styles page you can find a selection of different styles. If you click on one style, you "
+                    "will find information on the general trend of the style, as well as what tops, bottoms, "
+                    "accessories, and shoes to wear. As of now this page is incomplete. Just use the Chat to ask about"
+                    "other styles not mentioned on the styles page. The chatbot will give you information on any style "
+                    "you ask about. The tips page will give you some info on how to style outfits and where to get your"
+                    "clothes from. Have fun checking out this style guide!🤍")
